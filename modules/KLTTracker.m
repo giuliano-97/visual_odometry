2b00c458fe57ff69c129d291c6f0f91d905b61e6
Fausto Tapia <tfausto@student.ethz.ch>
2020-12-19 17:55:01 +0100

    commented KLT tracker

--- modules/KLTTracker.m
+++ modules/KLTTracker.m
@@ -1,4 +1,7 @@
 classdef KLTTracker
+% KLTTracker wrappers for matlab KLT Point Tracker
+%           Initializes tracker and calculates keypoints
+%           in the next frame
     properties
         tracker
     end
@@ -13,6 +16,17 @@ classdef KLTTracker
                                   'MaxIterations',max_iter);
         end
         function [points, validity, scores] = track(obj, img1, img2, img1_pts)
+            % track Obtains points tracked, validity and scores
+            %       of correspondences
+            % @param matrix img1:       gray scale image
+            % @param matrix img2:       gray scale image
+            % @param matrix img1_pts:   [N,2] float of detections in img1
+            % @output matrix points:    [N,2] float of points tracked form
+            %                           img1 to img2
+            % @output matrix validity   [N,1] bool representing validity of
+            %                           tracked points
+            % @output matrix score      [N,1] float scores for each track
+            
             initialize(obj.tracker, img1_pts, img1);
             [points, validity, scores] = obj.tracker(img2);
         end
