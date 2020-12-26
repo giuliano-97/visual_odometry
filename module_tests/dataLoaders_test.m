49506a0f111a755e562bcb5b0ea953f909989109
Fausto Tapia <ftstalin_csg@hotmail.com>
2020-12-26 17:24:53 +0100

    Added data loader for kitti data in VAMR tutorial, successsfull test is also included

--- module_tests/dataLoaders_test.m
+++ module_tests/dataLoaders_test.m
@@ -1,5 +1,5 @@
 
-dataset_type = 1; % 0: KITTI, 1: malaga, 2: parking, 3:KITTI_tutorial
+dataset_type = 3; % 0: KITTI, 1: malaga, 2: parking, 3:KITTI_tutorial
 
 % Pick the correspoinding data loader
 if dataset_type ==0
@@ -8,6 +8,10 @@ elseif dataset_type == 1
     data_loader = dataLoaderMalaga('./data/malaga-urban-dataset-extract-07');
 elseif dataset_type == 2
     data_loader = dataLoaderParking('./data/parking');
+elseif dataset_type == 3
+    data_loader = dataLoaderKittiTutorial('./data/continuous_op_test');
+else
+    assert(false, "Invalid dataset type choose: 0, 1,2,3");
 end
 
 % Iterate over all frames 
@@ -15,5 +19,5 @@ figure
 while ~data_loader.finished
    [img, gt_pose] = data_loader.next();
    imshow(img);
-   display(strcat(sprintf("GT position %08d: ", data_loader.index),"[",num2str(gt_pose),"]"))
+   display(strcat(sprintf("GT position %08d: ", data_loader.index-1),"[",num2str(gt_pose),"]"))
 end
---
+++ utils/dataLoaderKittiTutorial.m
@@ -0,0 +1,66 @@
+classdef dataLoaderKittiTutorial < handle
+    %DATALOADERPARKING data loader for parking dataset
+    properties
+        camParams
+        last_frame
+        index
+        dataset_path
+        img_file_format
+        ground_truth_data
+        initial_keypoints
+        initial_landmarks
+        finished
+    end
+    
+    methods
+        function obj = dataLoaderKittiTutorial(path)
+            %Parking dataset loader
+            obj.dataset_path = path;
+            [obj.camParams,...
+                obj.ground_truth_data,...
+                obj.initial_keypoints,...
+                obj.initial_landmarks,...
+                obj.last_frame] = obj.loadGeneralData();
+            obj.index = 0;
+            obj.img_file_format = strcat(obj.dataset_path,'/%06d.png');
+            obj.finished = false;
+        end
+        
+        % Gets next image and ground truth pose
+        function [next_img, gt_pose] = next(obj)
+            % Error if already loaded all files
+            assert(obj.index <= obj.last_frame);
+            next_img = imread(sprintf(obj.img_file_format,obj.index));
+            gt_pose = [];
+            obj.index = obj.index + 1;
+            if obj.index > obj.last_frame
+                obj.finished = true;
+            end
+        end
+        
+        % Retrieves a specific frame
+        function [img, ground_truth_pose] = retrieveFrame(obj, index)
+            % Error if invalid index
+            assert(index <= obj.last_frame);
+            img = imread(sprintf(obj.img_file_format,index));
+            ground_truth_pose = obj.ground_truth_data(obj.index+1, :);            
+        end
+        
+        % Load camera parameters gt_position and last_frame index
+        function [camParams, ground_truth, keypoints, landmarks, last_frame] = loadGeneralData(obj)
+            K = load(strcat(obj.dataset_path, '/K.txt'));
+            camParams = cameraParameters('IntrinsicMatrix', K.');
+            keypoints = load(strcat(obj.dataset_path, '/keypoints.txt'));
+            landmarks = load(strcat(obj.dataset_path, '/p_W_landmarks.txt'));
+            ground_truth = [];
+            last_frame = 9;
+        end
+        
+        % Resets object to the start point
+        function [] = reset(obj)
+            obj.index = 0;
+            obj.finished = false;
+        end
+    end
+end
+
