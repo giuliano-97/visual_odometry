c76e3d186be2c99ac62591a0639b5ba6dd26d3e6
Fausto Tapia <ftstalin_csg@hotmail.com>
2021-01-01 11:15:45 +0100

    Added different dataset support for KLT video test

--- module_tests/KLT_video_test.m
+++ module_tests/KLT_video_test.m
@@ -1,9 +1,6 @@
 close all;
 clear;
 
-% Fix random seed for reproducibility
-rng(1023);
-
 % Load data
 test_bootstrap = true;
 
@@ -28,17 +25,27 @@ klt_tracker = KLTTracker('NumPyramidLevels',4,...
                          'MaxBidirectionalError', 2,...
                          'BlockSize', [41, 41],...
                          'MaxIterations', 50);
+                     
+% Define number of frames to replay and where to start.
+num_frames = 200;
+start_frame = 1;
 
+% Initializing
+data_loader.reset(start_frame);
 prev_img = data_loader.next();
+if ndims(prev_img)>2
+    prev_img = rgb2gray(prev_img);
+end
 prev_keypoints = detectHarrisFeatures(prev_img, 'MinQuality', 0.0001,...
     'FilterSize', 3).Location;
-
 prev_img_marked = insertMarker(prev_img, prev_keypoints, '+','Color','white');
 imshow(prev_img_marked);
 
-num_frames = 200;
-for i = 2 : data_loader.index+num_frames-1
+for i = start_frame: start_frame+num_frames-1
     curr_img = data_loader.next();
+    if ndims(curr_img) > 2
+        curr_img = rgb2gray(curr_img);
+    end
     [curr_keypoints, valid] = klt_tracker.track(prev_img, curr_img,...
         prev_keypoints);
     curr_img_marked = insertMarker(curr_img, curr_keypoints(valid,:),...
